9e75ff9372c2c0cd335e68613099b3c2339af519
Bichngocc07 <ngoc01072006@gmail.com>
2026-01-27 19:26:11 +0700

    Implement book search feature in SS03_Bai2
    Added a book search functionality that allows users to find a book by its name in a predefined list.

--- Session03/src/SS03_Bai2.java
+++ Session03/src/SS03_Bai2.java
@@ -1,2 +1,32 @@
+import java.util.Scanner;
+
 public class SS03_Bai2 {
+
+   
+    public static int searchBooks(String[] arr, String search) {
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i].equals(search)) {  
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        
+        String[] books = {"Doraemon", "Lao Hac", "Chiec luoc nga", "Tat den", "Dac nhan tam"};
+
+        System.out.print("Nhập tên sách cần tìm: ");
+        String search = sc.nextLine();
+
+        int index = searchBooks(books, search);
+
+        if (index != -1) {
+            System.out.println("Tìm thấy sách '" + search + "' tại vị trí số: " + index);
+        } else {
+            System.out.println("Sách không tồn tại trong thư viện.");
+        }
+    }
 }
