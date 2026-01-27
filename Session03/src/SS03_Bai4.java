2a01651fdd3b782a565ffe41e57c9da7e2c2118c
Bichngocc07 <ngoc01072006@gmail.com>
2026-01-27 19:34:42 +0700

    Update SS03_Bai4.java

--- Session03/src/SS03_Bai4.java
+++ Session03/src/SS03_Bai4.java
@@ -1,2 +1,37 @@
 public class SS03_Bai4 {
+   
+    public static void sortBooks(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int tmp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = tmp;
+                }
+            }
+        }
+    }
+
+  
+    public static void displayBooks(int[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i]);
+            if (i < arr.length - 1) System.out.print(", ");
+        }
+        System.out.print("]");
+    }
+
+    public static void main(String[] args) {
+      
+        int[] books = {105, 102, 109, 101, 103};
+
+        System.out.print("Trước khi sắp xếp: ");
+        displayBooks(books);
+
+        sortBooks(books);
+
+        System.out.print("\nSau khi sắp xếp:   ");
+        displayBooks(books);
+    }
 }
