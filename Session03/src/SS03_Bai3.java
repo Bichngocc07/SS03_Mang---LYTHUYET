0686226ce2108eec47fdf6798ceaba398c4945ad
Bichngocc07 <ngoc01072006@gmail.com>
2026-01-27 19:31:30 +0700

    Implement methods to find max and min book quantities

--- Session03/src/SS03_Bai3.java
+++ Session03/src/SS03_Bai3.java
@@ -1,2 +1,50 @@
 public class SS03_Bai3 {
+
+    public static void maxQuantityOfBooks(String[] names, int[] quantities) {
+        int max = quantities[0];
+
+        
+        for (int i = 1; i < quantities.length; i++) {
+            if (quantities[i] > max) {
+                max = quantities[i];
+            }
+        }
+
+        System.out.println("Sách có số lượng nhiều nhất (" + max + "):");
+        
+        for (int i = 0; i < quantities.length; i++) {
+            if (quantities[i] == max) {
+                System.out.println("- " + names[i]);
+            }
+        }
+        System.out.println("--------------------");
+    }
+
+   
+    public static void minQuantityOfBooks(String[] names, int[] quantities) {
+        int min = quantities[0];
+
+     
+        for (int i = 1; i < quantities.length; i++) {
+            if (quantities[i] < min) {
+                min = quantities[i];
+            }
+        }
+
+        System.out.println("Sách có số lượng ít nhất (" + min + "):");
+  
+        for (int i = 0; i < quantities.length; i++) {
+            if (quantities[i] == min) {
+                System.out.println("- " + names[i]);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] names = {"Python Pro", "Web Design", "Data Structure", "Java Basic", "UI/UX"};
+        int[] quantities = {50, 50, 5, 20, 12};
+
+        maxQuantityOfBooks(names, quantities);
+        minQuantityOfBooks(names, quantities);
+    }
 }
