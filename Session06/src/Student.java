73149a22dfe958ab341b4f7516b9ff699b547ee3
Bichngocc07 <ngoc01072006@gmail.com>
2026-01-31 00:41:12 +0700

    Update Student.java

--- Session06/src/Student.java
+++ Session06/src/Student.java
@@ -4,16 +4,15 @@ public class Student {
     private int namSinh;
     private double diemTrungBinh;
 
-    // ===== Constructor có tham số =====
-    public Student(String maSV, String hoTen, int namSinh, double diemTrungBinh) {
+    
+public Student(String maSV, String hoTen, int namSinh, double diemTrungBinh) {
         this.maSV = maSV;
         this.hoTen = hoTen;
         this.namSinh = namSinh;
         this.diemTrungBinh = diemTrungBinh;
     }
-
-    // ===== Phương thức hiển thị thông tin =====
-    public void hienThiThongTin() {
+    
+public void hienThiThongTin() {
         System.out.println("Mã SV: " + maSV);
         System.out.println("Họ tên: " + hoTen);
         System.out.println("Năm sinh: " + namSinh);
@@ -21,3 +20,4 @@ public class Student {
         System.out.println("----------------------------");
     }
 }
+
