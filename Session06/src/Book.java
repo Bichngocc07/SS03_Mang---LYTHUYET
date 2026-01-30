cbb2a83516f7b87ea125e06678cd92076915fa34
Bichngocc07 <ngoc01072006@gmail.com>
2026-01-31 00:44:59 +0700

    Refactor Book class formatting and structure

--- Session06/src/Book.java
+++ Session06/src/Book.java
@@ -1,19 +1,16 @@
 public class Book {
-    // ===== Thuộc tính (instance variables) =====
     private String id;
     private String title;
     private String author;
     private double price;
 
-    // ===== Constructor (tham số TRÙNG tên thuộc tính) =====
     public Book(String id, String title, String author, double price) {
-        this.id = id;         // this.id  -> biến instance
-        this.title = title;   // title    -> tham số constructor
+        this.id = id;       
+        this.title = title;  
         this.author = author;
         this.price = price;
     }
 
-    // ===== Phương thức hiển thị =====
     public void display() {
         System.out.println("ID     : " + this.id);
         System.out.println("Title  : " + this.title);
@@ -22,3 +19,4 @@ public class Book {
         System.out.println("----------------------------");
     }
 }
+
