d348852e542d8474515392484f2abbe2fc2aec61
Bichngocc07 <ngoc01072006@gmail.com>
2026-02-02 23:31:48 +0700

    Add Student class with basic attributes and methods

--- Session07/src/BTTH.java
+++ Session07/src/BTTH.java
@@ -1,6 +1,44 @@
-public class BTTH {
-    public static void main(String[] args){
+public class Student {
 
+    
+    private String id;
+    private String name;
 
+    public static int studentCount = 0;
+
+    public static final String SCHOOL_NAME = "ABC University";
+
+    public Student(String id, String name) {
+        this.id = id;
+        this.name = name;
+        studentCount++;  
+    }
+
+  
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void displayInfo() {
+        System.out.println("Trường: " + SCHOOL_NAME);
+        System.out.println("Mã SV: " + id);
+        System.out.println("Tên SV: " + name);
+        System.out.println("-----------------------------");
+    }
+
+    public static void showTotalStudent() {
+        System.out.println("Tổng số sinh viên đã tạo: " + studentCount);
     }
 }
