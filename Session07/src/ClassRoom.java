3a3554a20d7be3e068b9f2f55d9c663bce3d647f
Bichngocc07 <ngoc01072006@gmail.com>
2026-02-02 23:26:08 +0700

    Refactor ClassRoom class structure

--- Session07/src/ClassRoom.java
+++ Session07/src/ClassRoom.java
@@ -1,27 +1,20 @@
 public class ClassRoom {
-
-    // ===== BIẾN DÙNG CHUNG CHO CẢ LỚP (STATIC) =====
     private static double classFund = 0.0;
 
-    // ===== BIẾN RIÊNG CỦA TỪNG SINH VIÊN (INSTANCE) =====
     private String studentName;
 
-    // Constructor
     public ClassRoom(String studentName) {
         this.studentName = studentName;
     }
-
-    // Sinh viên đóng tiền vào quỹ chung
     public void contribute(double amount) {
         if (amount > 0) {
-            classFund += amount;   // TÁC ĐỘNG VÀO BIẾN STATIC
+            classFund += amount;
             System.out.println(studentName + " đã đóng: " + amount);
         } else {
             System.out.println("Số tiền phải lớn hơn 0!");
         }
     }
 
-    // Xem tổng quỹ lớp (static vì thuộc về cả lớp)
     public static double getClassFund() {
         return classFund;
     }
@@ -30,3 +23,4 @@ public class ClassRoom {
         return studentName;
     }
 }
+
