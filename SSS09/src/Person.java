d8a457781485b7aa4a1e10b9983e9fd99fe7a489
Bichngocc07 <ngoc01072006@gmail.com>
2026-02-04 20:18:33 +0700

    Update Person.java

--- SSS09/src/Person.java
+++ SSS09/src/Person.java
@@ -1,17 +1,16 @@
-// Person.java
+
 public class Person {
     protected String name;
     protected int age;
 
-    // Constructor
     public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
 
-    // Phương thức hiển thị thông tin
     public void displayInfo() {
         System.out.println("Họ tên: " + name);
         System.out.println("Tuổi: " + age);
     }
 }
+
