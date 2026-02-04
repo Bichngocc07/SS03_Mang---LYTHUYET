06215b269467c4639ed2afbbc4e586de0acc9af5
Bichngocc07 <ngoc01072006@gmail.com>
2026-02-04 20:23:51 +0700

    Refactor Student class for improved readability

--- SSS09/src/Student.java
+++ SSS09/src/Student.java
@@ -1,20 +1,18 @@
-// Student.java
 public class Student extends Person {
     private String studentId;
     private double gpa;
 
-    // Constructor
     public Student(String name, int age, String studentId, double gpa) {
-        super(name, age); // gọi constructor lớp cha
+        super(name, age);
         this.studentId = studentId;
         this.gpa = gpa;
     }
 
-    // Ghi đè phương thức hiển thị thông tin
     @Override
     public void displayInfo() {
-        super.displayInfo(); // dùng lại code lớp cha
+        super.displayInfo(); 
         System.out.println("Mã sinh viên: " + studentId);
         System.out.println("Điểm trung bình: " + gpa);
     }
 }
+
